f2ddbc5a4d701519ee7d367294c3ae6e0c56277f
astrider.com@gmail.com <astrider.com@gmail.com>
2012-10-11 15:04:59 +0900

    fixed strReg

--- TUS_Line.php
+++ TUS_Line.php
@@ -4,7 +4,7 @@
         private $tokenCount = 0;
         private $text;
         private $numReg ="/[0-9]+/";
-        private $strReg ="/[A-Z_a-z][A-Z_a-z0-9]*|==|<=|>=|&&|\|\||p{Punct}/";
+        private $strReg ="/[A-Z_a-z][A-Z_a-z0-9]*|==|<=|>=|&&|\|\||[[:punct:]]/";
         private $idenReg = '/(\\"|\\\\|\\n|[^"])*/';
         private $tokens;
         
