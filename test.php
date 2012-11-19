e7671eaac43a9cb4acd2a0ff35db54bcd61e3cbf
Tran Duc Thang <thangtd90@gmail.com>
2012-11-19 16:05:23 +0900

    Edit scipt.tus and test file for testing purpose

--- test.php
+++ test.php
@@ -5,15 +5,17 @@
     include (__DIR__.'/TUS_ASTree.php');
     include (__DIR__.'/TUS_ExprParser.php');
     include (__DIR__.'/TUS_OpPrecedenceParser.php');
-    include (__DIR__.'/TUS_BasicParser.php');        
+    include (__DIR__.'/TUS_BasicParser.php');
+    include (__DIR__.'/TUS_BasicEnv.php'); 
     
     $filepath = __DIR__."/script.tus";
-    $fileReader = new TUS_FileReader($filepath);    
-    //$p1 = new TUS_ExprParser($fileReader);
-    //print_r($p1->expression());
-    //$fileReader->setCurrent(0);
+    $fileReader = new TUS_FileReader($filepath);        
+    $p = new TUS_BasicParser($fileReader);
+    print_r($p->parse());
     
-    $p2 = new TUS_BasicParser($fileReader);
-    $tree = $p2->parse();
-    print_r ($tree);
+    $fileReader->setCurrent(0);
+    $p = new TUS_BasicParser($fileReader);    
+    $env = new TUS_BasicEnv();
+    $p->evaluate($env);
+    print_r($env);
 ?>
