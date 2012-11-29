c6a08602360732bc8c53f20108fbe004c0af88ed
wataridori <thangtd90@gmail.com>
2012-11-29 14:11:49 +0900

    fix bug at class TUS_Name

--- TUS_ASTree.php
+++ TUS_ASTree.php
@@ -33,7 +33,7 @@ class TUS_ASTLeaf{
 
 class TUS_Name extends TUS_ASTLeaf{        
     function evaluate($env) {
-        return $env->get($this->name());
+        return $env->get($this->toString());
     }
 }
 
