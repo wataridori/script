f9231e51c5ce25874ba16bddb812c46ac97326f0
Tran Duc Thang <thangtd90@gmail.com>
2012-12-04 12:23:12 +0900

    Remove ASTree.php, create Function Parser and Function Interpreter

---
+++ TUS/TUS_Function.php
@@ -0,0 +1,148 @@
+<?php
+    class TUS_PostFix extends TUS_ASTList{
+        function __construct($list = null) {
+            $this->children = $list;
+        }
+    }
+    
+    class TUS_Arguments extends TUS_PostFix {
+        function size() {
+            return $this->numChildren();
+        }        
+    }        
+    
+    class TUS_Function{
+        protected $params, $body, $env;
+        function __construct($params, $body, $env) {
+            $this->params = $params;
+            $this->body = $body;
+            $this->env = $env;
+        }
+        
+        function makeEnv(){
+            return new TUS_BasicEnv($this->env);
+        }
+        
+        function parameters(){
+            return $this->params;
+        }
+        
+        function body(){
+            return $this->body;
+        }
+    }
+    
+    class TUS_FuncParser extends TUS_BasicParser{        
+        function __construct($file){            
+            $this->file = $file;            
+        }
+        
+        function param(){
+            $p = $this->file->read();
+            if ($p->isIdentifier()) {
+                return new TUS_Name($p);
+            } else $this->throwError($p);
+        }
+        
+        function params(){
+            $left = $this->param();
+            while ($this->isToken(",")){
+                $p = new TUS_ASTLeaf($this->file->read());
+                $right = $this->param();
+                $left = new TUS_Arguments(array($left,$right));            
+            }
+            return $left;
+        }
+        
+        function paramList(){
+            $this->token ("(");
+            if ($this->isToken(")")) {                
+                $p = new TUS_NullStmnt();
+            } else {                
+                $p = $this->params();
+            }
+            $this->token(")");
+            return $p;
+        }
+        
+        function funcDef(){
+            $this->token("def");
+            $t = $this->file->read();            
+            $funcName = new TUS_Name($t);            
+            $funcParamList = $this->paramList();            
+            $funcBody = $this->block();
+            return new TUS_DefStmnt(array($funcName,$funcParamList,$funcBody));
+        }
+        
+        function args() {
+            $left = $this->equation();
+            while ($this->isToken(",")) {
+                $t = new TUS_ASTLeaf($this->file->read());
+                $right = $this->equation();
+                $left = new TUS_ASTList(array($left,$right));
+            }
+            return $left;
+        }
+        
+        function postfix(){
+            $this->token("(");
+            if ($this->isToken(")")) {
+                $arg = new TUS_NullStmnt();
+            } else {
+                $arg = $this->args();
+            }
+            $this->token(")");
+            return $arg;
+        }
+        
+        function primary(){
+            if ($this->isToken("(")) {
+                $this->token("(");
+                $left = $this->equation();
+                $this->token(")");
+            } else {
+                $t = $this->file->read();
+                if ($t->isIdentifier()) {
+                    $left = new TUS_Name($t);
+                } else if ($t->isNumber() || $t->isString()) {
+                    $left = new TUS_ASTLeaf($t);
+                } else {
+                    $this->throwError($t);
+                }
+            }
+            
+            if ($this->isToken("(")) {
+                $right = $this->postfix();
+                return new TUS_PrimaryExpr(array ($left,$right));
+            } else {
+                return new TUS_PrimaryExpr(array($left));
+            }
+        }
+        
+        function simple(){
+            $left = $this->equation();
+            if ($this->isToken("(")) {
+                $right = $this->postfix();
+                $left = new TUS_ASTList(array($left,$right));
+            }
+            return $left;
+        }
+        
+        function program(){            
+            if ($this->isToken(";") || $this->isToken(TUS_Token::EOL)) {                
+                $this->file->read();
+                return new TUS_NullStmnt();
+            } else {                
+                if ($this->isToken("def")) {                    
+                    $fd = $this->funcDef();                    
+                    $this->file->read();                    
+                    return $fd;
+                } else {                    
+                    $stmn = $this->statement();
+                    $this->file->read();
+                    return $stmn;
+                }
+            }
+        }                
+    }        
+?>
