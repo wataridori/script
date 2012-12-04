cbf97e60e9c8eb9c8d60e4f3b839c095242b9b1b
Tran Duc Thang <thangtd90@gmail.com>
2012-12-04 16:13:09 +0900

    For testing purpose

--- test-funcParser.php
+++ test-funcParser.php
@@ -2,7 +2,6 @@
     include (__DIR__."/TUS/main.php");    
     $filepath = __DIR__."/funcParser.tus";    
     $fileReader = new TUS_FileReader($filepath);                        
-    $p = new TUS_FuncParser($fileReader);        
-    $env = new TUS_BasicEnv();
-    print_r($p->parse()); 
+    $p = new TUS_FuncParser($fileReader);            
+    print_r($p->parse());            
 ?>
