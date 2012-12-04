d041fa6fe1736c3b62c2965abfccb3095ad108ba
Tran Duc Thang <thangtd90@gmail.com>
2012-12-04 15:49:55 +0900

    New functions to check, set or get Parent Enviroment of an enviroment

--- TUS/TUS_BasicEnv.php
+++ TUS/TUS_BasicEnv.php
@@ -1,17 +1,36 @@
 <?php
 class TUS_BasicEnv{
     protected $values;
+    protected $pe = null;
     function __construct($defaultValue = null){
         if ($defaultValue == null) $defaultValue = new ArrayObject;
         $this->values = $defaultValue;
+    }
+    
+    function setParentEnv($env){
+        $this->pe = $env;
+    }
+    
+    function hasParentEnv(){
+        if ($this->pe == null) return false;
+        else return true;
+    }
+    
+    function getParentEnv(){
+        return $this->pe;
     }
     
     function put($name,$value){
         $this->values[$name] = $value;
     }
     
-    function get($name) {
-        return $this->values[$name];
+    function get($name) {        
+        if (array_key_exists($name,$this->values))
+            return $this->values[$name];
+        else if ($this->hasParentEnv()){            
+            return $this->getParentEnv()->get($name);        
+        }
+        else return null;
     }    
 }
 ?>
