933cfef518343e4b1fe83b3a2d86a23941a90f3c
Tran Duc Thang <thangtd90@gmail.com>
2012-12-25 18:18:11 +0900

    For testing purpose

---
+++ test-BuiltinFunction.php
@@ -0,0 +1,8 @@
+<?php
+    include (__DIR__."/TUS/main.php");    
+    $filepath = __DIR__."/builtinFunc.tus";    
+    $fileReader = new TUS_FileReader($filepath);                        
+    $p = new TUS_FuncParser($fileReader);            
+    $env = new TUS_BasicEnv();    
+    $p->evaluate($env);
+?>
